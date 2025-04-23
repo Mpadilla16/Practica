2a285e4fa09637c0439f13cca0de824502b700de
Mpadilla16 <mariaangpadilla@gmail.com>
2025-04-23 15:11:25 -0500

    javascrip

--- HTML/Menu_lateral_2/script.js
+++ HTML/Menu_lateral_2/script.js
@@ -1,1 +1,13 @@
-console.log('Hola');
+const menuItemDropDow = document.querySelectorAll(".menu-item-dropdow");
+
+menuItemDropDow.forEach((menuItem)=>{
+    menuItem.addEventListener('click'()=>{
+        const subMenu = menuItem.querySelector('.sub-menu');
+        menuItem.class  {
+            constructor(parameters) {
+                
+            }
+        }
+    });
+
+});
